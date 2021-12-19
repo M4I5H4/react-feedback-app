01fe633d1f832005226eefca95398284a602a558
M4I5HA <maisha.adamsam@gmail.com>
2021-12-19 22:51:33 +0000

    added new files

--- react-feedback-app/src/App.js
+++ react-feedback-app/src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+
 function App() {
-  return <h1>Hello from the app componet </h1>;
+  
+  return (
+    <>
+    <Header />
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+    </>
+  );
 }
 
-export default App
+Header.defaultProps = {
+  text: 'Feedback UI'
+}
+
+export default App;
